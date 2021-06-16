4e27f12865a2f49a0a7360b276556f0aa25ac27a
Ian Graham <irgraham1@gmail.com>
2021-06-16 14:25:06 -0400

    Refactor test

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # gsd-sys
-This crate presents a native FFI to GSD's C API.
+This crate presents an FFI to GSD's native C API. The GSD (General Simulation Data)
+file format is the native file format of [HOOMD-blue](https://glotzerlab.engin.umich.edu/hoomd-blue/)
+and is used to store and analyze HOOMD-blue simulations. Though it's also a perfectly good binary file
+format for chunk-like data, so why not wrap Rust around it?
 
-* [Documentation](https://gsd.readthedocs.io/en/stable/c-api.html)
+Original documentation for the API can be found 
+[here](https://gsd.readthedocs.io/en/stable/c-api.html).
 
 ## Usage
 To add `gsd-sys` as a dependency in `Cargo.toml`, please clone the repository
-onto your system and it as a path dependency:
+onto your system and use it as a path dependency (until I get around to
+publishing it on crates.io alongside a high-level API):
 
 ```toml
 [dependencies]
--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,37 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 mod tests {
     use super::*;
     use std::ffi::CString;
-    use std::fs::remove_file;
+    use std::fs::remove_file;  
+    use std::io::ErrorKind;
+    use std::env::temp_dir;
+    use std::string::String;
+
+    fn safely_remove_file_if_exists(file: &String) {
+        match remove_file(file) {
+            Ok(()) => (),
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => (),
+                other_error => {
+                    panic!("Problem removing the file: {:?}", other_error)
+                }
+            }
+        }
+    }
 
     #[test]
     fn create_and_remove_file() {
 
-        let rusty_fname = "src/test.gsd";
-        let fname = CString::new(rusty_fname).expect("CString::new failed");
+        let rusty_fname = format!(
+            "{}/test.gsd",
+            temp_dir().into_os_string().into_string().unwrap()
+        );
+        safely_remove_file_if_exists(&rusty_fname);
+
+        let fname = CString::new(rusty_fname.clone()).expect("CString::new failed");
         let app = CString::new("gsd-sys").expect("CString::new failed");
         let schema = CString::new("test").expect("CString::new failed");
         let schema_version: u32 = 0;
+
         unsafe {
             let res = gsd_create(
                 fname.as_ptr(),
@@ -26,9 +47,15 @@ mod tests {
                 schema.as_ptr(),
                 schema_version
             );
-            assert_eq!(res, 0) // checks that file was created without issue
+            assert_eq!(res, gsd_error_GSD_SUCCESS) // checks that file was created without issue
         }
 
-        remove_file(rusty_fname).expect("Failed to remove gsd file");
+        safely_remove_file_if_exists(&rusty_fname)
     }
+
+    #[test]
+    fn write_and_read_file() {
+        
+    }
+
 }
